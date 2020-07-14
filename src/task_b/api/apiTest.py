ca829cb79db2452e00ad32e11946acbe11f4bba2
zd247 <zduy2407@gmail.com>
2020-07-14 19:23:05 +0700

    minor changes this afternoon

--- src/task_b/api/apiTest.py
+++ src/task_b/api/apiTest.py
@@ -1,13 +1,50 @@
 from src.task_b.api.apiRESTful import ApiRESTful as api
 from sense_hat import SenseHat
 from res.container import Container as c
+import json
 
 # auto
 class ReadAndDisplay():
    def __init__(self):
       self.sense = SenseHat()
       self.api = api()
-      
+      self.testPasses = [False, False, False, False]
+
+   def testGetAllData(self): 
+      data = self.api.getAllData()
+      if (data.count > 0 or data is not None):
+         print (data)
+         self.testPasses[0] = True
+
+   def testGetLastData (self):
+      data = self.api.getLastData()
+      if (data is not None):
+         print (data)
+         self.testPasses[1] = True
    
+   def testPostData(self):
+      self.api.postData(9999,9999)
+      newData = json.loads(self.api.getLastData())[0]
+      if (newData['temp'] is 9999 and newData['humidity'] is 9999):
+         self.testPasses[2] = True
+      
+   def testUpdateLastData(self):
+      self.api.updateLastData(6666, 8888)
+      lastData = json.loads(self.api.getLastData())[0]
+      if (lastData['temp'] is 6666 and lastData['humidity'] is 8888):
+         self.testPasses[3] = True
+   
+
    def execute(self):
-      return 0
+      self.testGetAllData()
+      self.testGetLastData()
+      self.testPostData()
+      self.testUpdateLastData()
+
+      count = 0
+      for i in range (self.testPasses.count):
+         if (self.testPasses[i] is True):
+            count += 1
+      
+      print ("Number of test passes {}/{}".format(count, self.testPasses.count))
+      return
--- src/task_b/createReport.py
+++ src/task_b/createReport.py
@@ -1,9 +1,18 @@
 import json
+from src.task_b.monitorAndNotify import MonitorAndNotify as mntf
+from res.container import Container as c
+
 
 class CreateReport():
-   
+   def __init__(self):
+      self.mntf = mntf()
+
+
    def readDailyData(self):
-      return 0
+      temp, humidity = self.mntf.readDataFromSense()
+      if temp < c.min or temp > c.max:
+         
+
 
    def writeReport(self):
       return 1
